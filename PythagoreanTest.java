8563ef6ca2bda2e2992e08ec93cc844fe1160c57
Christopher Guerrero <71108741+Chrisgrr3@users.noreply.github.com>
2020-11-27 11:10:51 -0900

    Add files via upload

---
+++ PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean pyth = new Pythagorean();
+        double hypotenuse = pyth.calculateHypotenuse(3, 4);
+        System.out.println(hypotenuse);
+    }
+}
